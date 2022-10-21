c5c475b725ef04c0211a6df94a57c31d4281346b
Eric Knorr <eric_knorr@g.harvard.edu>
2022-10-21 10:33:19 -0400

    Document in-memory benchmark script

--- bench/bench.sh
+++ bench/bench.sh
@@ -1,9 +1,101 @@
 #!/bin/bash
 
+###############################################
+###########   WORKLOAD PARAMETERS   ###########
+###############################################
+
+## NOTE: paired values
+#  By default, we loop through all combinations of parameters, but paired values 
+#  are considered as a single parameter.  i.e. 
+#
+#       kdist_arr=("kuniform" "kuniform")
+#       qdist_arr=("quniform" "qcorrelated")
+#
+#  Corresponds to two workloads with uniform key distributions, one with 
+#  uniform queries and the other with correlated queries.
+
+
+## Key Length (in bits)
+# For integer workloads, this value MUST be 64
+# For synthetic string workloads, this value MUST be a multiple of 8
+# For the string domain workload, this value is ignored.
+
+keylen_arr=(64) # int
+# keylen_arr=(1440) # str
+
+## Number of Keys and Queries [PAIRED]
+nkeys_arr=(10000000)
+nqrys_arr=(1000000) 
+
+## Key and Query Distribution [PAIRED]
+
+# For SOSD / Domain workloads, please download the relevant datasets and follow the instructions in the README
+# SOSD and Domain Key and Query Distributions must be specified together, e.g. ksosd_books + qsosd_books
+
+# Integer Key Distributions: kuniform, knormal, ksosd_books, ksosd_fb
+# Integer Query Distributions: quniform, qnormal, qcorrelated, qsplit, qsosd_books, qsosd_fb
+
+# String Key Distributions: kuniform, knormal, kdomain
+# String Query Distributions: quniform, qcorrelated, qsplit, qdomain
+
+kdist_arr=("kuniform" "kuniform" "knormal" "knormal" "knormal")
+qdist_arr=("quniform" "qcorrelated" "qsplit" "qcorrelated" "quniform")
+
+## Min and Max Range Size [PAIRED]
+# Min Range Size must be >= 2
+minrange_arr=(2) 
+maxrange_arr=(4096)     # int
+# maxrange_arr=(1073741824)   # str
+
+## Point / Range Query Ratio
+# Must be >= 0.0 && <= 1.0
+# Use 0.0 for all range queries and 1.0 for all point queries
+pqratio_arr=(0.0)
+
+## Positive / Negative Query Ratio
+# Must be >= 0.0 && <= 1.0
+# Use 0.0 for all negative queries and 1.0 for all positive queries
+pnratio_arr=(0.0) 
+
+## Query Correlation Degree
+# The distance between the left query bound and a key is uniformly distributed between 1 and the specified value
+# Must be >= 1
+corrd_arr=(1024)    # int
+# corrd_arr=(536870912)   # str
+
+
+###############################################
+############   FILTER PARAMETERS   ############
+###############################################
+
+
+### Proteus
+
+## Filter Bits-per-Key
+# Must be a positive real number
+membudg_arr=(8 10 12 14 16 18)
+
+## Determines the proportion of queries given to Proteus as a sample
+# Must be >= 0.0 && <= 1.0
+# Default sample size for in-memory benchmarks is 20K, see paper for sample size justification
+samplerate_arr=(0.02)
+
+### SuRF 
+
+## Hash and Real Suffix Lengths
+# These values must be non-negative integers and they control SuRF's memory usage.
+# Hash suffixes are only useful for point queries 
+surfhlen_arr=(0)
+surfrlen_arr=(2)
+
+
+################################################################
+
 REPO_DIR="$(pwd)"
-SOSD_DIR="$REPO_DIR/../SOSD/"
-INT_WORKL_BIN="$REPO_DIR/../int_workload"
-STR_WORKL_BIN="$REPO_DIR/../str_workload"
+SOSD_DIR="$REPO_DIR/../workloads/SOSD/"
+DOMAINS_DIR="$REPO_DIR/../workloads/domains/"
+INT_WORKL_BIN="$REPO_DIR/../workloads/int_workload_gen"
+STR_WORKL_BIN="$REPO_DIR/../workloads/str_workload_gen"
 EXP_BIN="$REPO_DIR/bench"
 
 IS_INT_BENCH="$1"
@@ -14,17 +106,7 @@ else
 fi
 
 experiment() {
-    nkeys=$1 && shift
-    keylen=$1 && shift
-    nqrys=$1 && shift
-    kdist=$1 && shift
-    qdist=$1 && shift
-    pqratio=$1 && shift
-    membudg=$1 && shift
     filter=$1 && shift
-    expdir=$1 && shift
-    corrd=$1 && shift
-    pnratio=$1 && shift
 
     cd "$expdir"
     touch ./experiment_result
@@ -33,39 +115,35 @@ experiment() {
     else
         echo -e "### BEGIN STRING EXPERIMENT ###" >> ./experiment_result
     fi
-    # echo -e "$expdir" >> ./experiment_result
     echo -e "### BEGIN EXPERIMENT DESCRIPTION ###" >> ./experiment_result
-    echo -e "\tFilter name:\t$filter" >> ./experiment_result
-    echo -e "\tNumber of keys:\t$nkeys" >> ./experiment_result
-    echo -e "\tNumber of bits in a key:\t$keylen" >> ./experiment_result
-    echo -e "\tNumber of queries:\t$nqrys" >> ./experiment_result
-    echo -e "\tKey distribution:\t$kdist" >> ./experiment_result
-    echo -e "\tQuery distribution:\t$qdist" >> ./experiment_result
-    echo -e "\tMinimum range:\t$minrange" >> ./experiment_result
-    echo -e "\tMaximum range:\t$maxrange" >> ./experiment_result
-    echo -e "\tPoint-Query ratio:\t$pqratio" >> ./experiment_result
-    echo -e "\tFilter bits-per-key:\t$membudg" >> ./experiment_result
-    echo -e "\tCorrelation degree:\t$corrd" >> ./experiment_result
-    echo -e "\tPositive-Negative-Query ratio:\t$pnratio" >> ./experiment_result
+    echo -e "\tFilter Name:\t$filter" >> ./experiment_result
+    echo -e "\tNumber of Keys:\t$nkeys" >> ./experiment_result
+    echo -e "\tKey Length:\t$keylen" >> ./experiment_result
+    echo -e "\tNumber of Queries:\t$nqrys" >> ./experiment_result
+    echo -e "\tKey Distribution:\t$kdist" >> ./experiment_result
+    echo -e "\tQuery Distribution:\t$qdist" >> ./experiment_result
+    echo -e "\tMinimum Range Size:\t$minrange" >> ./experiment_result
+    echo -e "\tMaximum Range Size:\t$maxrange" >> ./experiment_result
+    echo -e "\tPoint / Range Query Ratio:\t$pqratio" >> ./experiment_result
+    echo -e "\tQuery Correlation Degree:\t$corrd" >> ./experiment_result
+    echo -e "\tPositive / Negative Query ratio:\t$pnratio" >> ./experiment_result
     
     if [ $IS_INT_BENCH = "1" ]; then
         echo "$filter" "$INT_WORKL_BIN" "$SOSD_DIR" "$nkeys" "$nqrys" "$minrange" "$maxrange" "$kdist" "$qdist" "$pqratio" "$pnratio" "$corrd"
         $INT_WORKL_BIN "$SOSD_DIR" "$nkeys" "$nqrys" "$minrange" "$maxrange" "$kdist" "$qdist" "$pqratio" "$pnratio" "$corrd"
     else
-        echo "$filter" "$STR_WORKL_BIN" "$nkeys" "$keylen" "$nqrys" "$minrange" "$maxrange" "$kdist" "$qdist" "$pqratio" "$pnratio" "$corrd"
-        $STR_WORKL_BIN "$nkeys" "$keylen" "$nqrys" "$minrange" "$maxrange" "$kdist" "$qdist" "$pqratio" "$pnratio" "$corrd"
+        echo "$filter" "$STR_WORKL_BIN" "$DOMAINS_DIR" "$nkeys" "$keylen" "$nqrys" "$minrange" "$maxrange" "$kdist" "$qdist" "$pqratio" "$pnratio" "$corrd"
+        $STR_WORKL_BIN "$DOMAINS_DIR" "$nkeys" "$keylen" "$nqrys" "$minrange" "$maxrange" "$kdist" "$qdist" "$pqratio" "$pnratio" "$corrd"
     fi
     
     if [ $filter = "SuRF" ]; then
-        surfhlen=$1 && shift
-        surfrlen=$1 && shift
-        echo -e "\tSurf hash suffix length:\t$surfhlen" >> ./experiment_result
-        echo -e "\tSurf real suffix length:\t$surfrlen" >> ./experiment_result
+        echo -e "\tSuRF Hash Suffix Length:\t$surfhlen" >> ./experiment_result
+        echo -e "\tSuRF Real Suffix Length:\t$surfrlen" >> ./experiment_result
         echo -e "### END EXPERIMENT DESCRIPTION ###\n\n" >> ./experiment_result
-        $EXP_BIN "$IS_INT_BENCH" "$nkeys" "$keylen" "$nqrys" "$membudg" "$filter" "$surfhlen" "$surfrlen" >> ./experiment_result
+        $EXP_BIN "$IS_INT_BENCH" "$nkeys" "$keylen" "$nqrys" "10" "$filter" "$surfhlen" "$surfrlen" >> ./experiment_result
     
     elif [ $filter = "Proteus" ]; then
-        samplerate=$1 && shift
+        echo -e "\tFilter Bits-per-Key:\t$membudg" >> ./experiment_result
         echo -e "\tSample Rate:\t$samplerate" >> ./experiment_result
         echo -e "### END EXPERIMENT DESCRIPTION ###\n\n" >> ./experiment_result
         $EXP_BIN "$IS_INT_BENCH" "$nkeys" "$keylen" "$nqrys" "$membudg" "$filter" "$samplerate" >> ./experiment_result
@@ -79,53 +157,11 @@ experiment() {
     rm experiment_result
 }
 
-# STRING
-
-# keylen_arr=(1440) # MUST be a multiple of 8 for strings
-# nkeys_arr=(10000000)
-# kdist_arr=("kuniform" "kuniform" "knormal" "knormal")
-# nqrys_arr=(1000000) 
-# qdist_arr=("quniform" "qcorrelated" "qsplit" "qcorrelated")
-# minrange_arr=(2) 
-# maxrange_arr=(1073741824)
-# pqratio_arr=(0.0) # Must be >= 0.0 && <= 1.0
-# pnratio_arr=(0.0) # Must be >= 0.0 && <= 1.0
-# corrd_arr=(536870912) # Must be >= 1
-
-################################################################
-
-# INTEGER
-
-keylen_arr=(64) # Should only be 64
-nkeys_arr=(10000000)
-kdist_arr=("kuniform" "kuniform" "knormal" "knormal" "knormal")
-nqrys_arr=(1000000) 
-qdist_arr=("quniform" "qcorrelated" "qsplit" "qcorrelated" "quniform")
-minrange_arr=(2) 
-maxrange_arr=(4096)
-pqratio_arr=(0.0) # Must be >= 0.0 && <= 1.0
-pnratio_arr=(0.0) # Must be >= 0.0 && <= 1.0
-corrd_arr=(1024) # Must be >= 1
-
-################################################################
-
-membudg_arr=(8 10 12 14 16 18)
-surfhlen_arr=(0)
-surfrlen_arr=(2)
-samplerate_arr=(0.02)
-
-expdir_arr=()
-
-
 # Proteus Experiments
 for nkeys in "${nkeys_arr[@]}"; do
 for keylen in "${keylen_arr[@]}"; do   
 for nqrys in "${nqrys_arr[@]}"; do
-# for kdist in "${kdist_arr[@]}"; do
-# for qdist in "${qdist_arr[@]}"; do
 for i in "${!kdist_arr[@]}"; do
-# for minrange in "${minrange_arr[@]}"; do
-# for maxrange in "${maxrange_arr[@]}"; do
 for j in "${!minrange_arr[@]}"; do
 for pqratio in "${pqratio_arr[@]}"; do
 for membudg in "${membudg_arr[@]}"; do
@@ -138,11 +174,9 @@ qdist="${qdist_arr[$i]}"
 minrange="${minrange_arr[$j]}"
 maxrange="${maxrange_arr[$j]}"
 
-expdir=$(mktemp -d /tmp/proteus_experiment.XXXXXXX)
-expdir_arr+=("$expdir")                                                
-experiment $nkeys $keylen $nqrys $kdist $qdist $pqratio $membudg "Proteus" $expdir $corrd $pnratio $samplerate
+expdir=$(mktemp -d /tmp/proteus_exp.XXXXXXX)
+experiment "Proteus"
 
-# done
 done
 done
 done
@@ -159,14 +193,9 @@ done
 for nkeys in "${nkeys_arr[@]}"; do
 for keylen in "${keylen_arr[@]}"; do
 for nqrys in "${nqrys_arr[@]}"; do
-# for kdist in "${kdist_arr[@]}"; do
-# for qdist in "${qdist_arr[@]}"; do
 for i in "${!kdist_arr[@]}"; do
-# for minrange in "${minrange_arr[@]}"; do
-# for maxrange in "${maxrange_arr[@]}"; do
 for j in "${!minrange_arr[@]}"; do
 for pqratio in "${pqratio_arr[@]}"; do
-for membudg in 10; do
 for corrd in "${corrd_arr[@]}"; do
 for pnratio in "${pnratio_arr[@]}"; do
 for surfhlen in "${surfhlen_arr[@]}"; do
@@ -177,11 +206,9 @@ qdist="${qdist_arr[$i]}"
 minrange="${minrange_arr[$j]}"
 maxrange="${maxrange_arr[$j]}"
 
-expdir=$(mktemp -d /tmp/surf_experiment.XXXXXXX)
-expdir_arr+=("$expdir")
-experiment $nkeys $keylen $nqrys $kdist $qdist $pqratio $membudg "SuRF" "$expdir" $corrd $pnratio $surfhlen $surfrlen
+expdir=$(mktemp -d /tmp/surf_exp.XXXXXXX)
+experiment "SuRF"
 
-# done
 done
 done
 done
